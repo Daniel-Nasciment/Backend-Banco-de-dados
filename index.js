feaa8f07e1e94a15d06f444d9be16a21de866064
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-03-23 20:38:51 -0300

    Testes

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ const config = require('config');
 
 const port = process.env.PORT || config.get('server.port');
 
+const pedido = {
+    mensagem: 'Teste de pedido'
+}
+
+const registro = {
+    mensagem: 'Teste de registro'
+}
+
 const app = express();
+
+
 
 app.set('port', port);
 
-app.route('/pedido'.get((req, res) => {
-    res.status(200).env("Teste de pedidos OK")
-}));
+app.route('/pedido').get((req, res) => {
+    res.status(200).json(pedido)
+});
 
-app.route('/registro'.get((req, res) => {
-    res.status(200).env("Teste de Registro OK")
-}));
+app.route('/registro').get((req, res) => {
+    res.status(200).json(registro);
+});
 
 app.listen(port, () => {
     console.log("Porta: " + port)
