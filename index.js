8d1e2f22d5b9db3700d7a4dc257bb202fd464bcc
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-03-23 20:12:23 -0300

    Configurações iniciais

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const path = require('path');
 const config = require('config');
+
+
+const port = process.env.PORT || config.get('server.port');
+
+const app = express();
+
+app.set('port', port);
+
+app.route('/pedido'.get((req, res) => {
+    res.status(200).env("Teste de pedidos OK")
+}));
+
+app.route('/registro'.get((req, res) => {
+    res.status(200).env("Teste de Registro OK")
+}));
+
+app.listen(port, () => {
+    console.log("Porta: " + port)
+})
